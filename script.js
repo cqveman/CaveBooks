c3165475a610b4b55fa7e4a847974deab43f8969
cqveman <104222495+cqveman@users.noreply.github.com>
2023-10-12 16:21:24 +0300

    Creating a Book object

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    const library = [];
+
+    function Book(name, author, pages, published, read) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.published = published;
+        this.read = read;
+    }
 });
