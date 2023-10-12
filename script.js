52e229c65d75bae42029b715a29bc3a950deb074
cqveman <104222495+cqveman@users.noreply.github.com>
2023-10-12 16:38:26 +0300

    Adding addBookToLibrary function
    The function prompt the user for a book name then it adds it to the library array

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const library = [];
+	const library = [];
 
-    function Book(name, author, pages, published, read) {
-        this.name = name;
-        this.author = author;
-        this.pages = pages;
-        this.published = published;
-        this.read = read;
-    }
+	function Book(name, author, pages, published, read) {
+		this.name = name;
+		this.author = author;
+		this.pages = pages;
+		this.published = published;
+		this.read = read;
+	}
+
+	function addBookToLibrary() {
+		let newBook = prompt("Enter a book name!");
+        library.push(newBook);
+        return console.log(library);
+	}
+
+    addBookToLibrary()
 });
